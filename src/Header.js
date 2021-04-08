c3b646a28eea8bc78652a7c46008149a9cdf7555
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-04-08 12:02:02 +0530

    responsive header with menu burger

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Header.css'
 import {Link} from 'react-router-dom'
 import SearchIcon from '@material-ui/icons/Search' 
@@ -6,47 +6,65 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import { useSelector } from 'react-redux';
 import { initialState } from './reducers/reducer';
  
 function Header() {
 
-  const cart = useSelector(state => state.items)
+  const cart = useSelector(state => state.items);
+  const [open, setOpen] = useState(false);
 
+  const openMenu = () =>{
+    setOpen(!open); 
+  }
 
   return (
     <div className='header'>
+    <div className='lcheader'>  
       <Link className='header-logo-link' to='/'>
          <span className='mgr'>GowthaM's</span>
          <span className='mgr-icon'> <HeadsetMicIcon /></span>
       </Link>
-      <div className='header-searchbar'>
+      <div className='header-searchbar-sb'>
         <input className='h-sb-inp' placeholder='search' />
         <SearchIcon className='h-sb-icon' />
       </div>
-      <div className='header-navs'>
-          <Link className='header-navs-link one' to='login'>
+      <div className='header-navs'> 
+          <Link className='header-navs-link one disphide' to='login'>
               <span className='h-n-l-onespan'>Hello Customer</span>
               <span className='h-n-l-twospan'>Sign in</span>
           </Link>
-          <Link className='header-navs-link two' to='/'>
-              <span className='h-n-l-onespan'>Returns</span>
-              <span className='h-n-l-twospan'>& orders</span>
-          </Link> 
-          <Link className='header-navs-link two' to='/'>
+          <Link className='header-navs-link two disphide' to='/'>
             <NotificationsIcon className='notif' /> 
           </Link>  
-          <Link className='header-navs-link-last cart' to='checkout'>
-              <span className='h-n-l-onespan h-n-l-onespan-white'>CART</span> 
-              <ShoppingCartIcon className='h-n-l-l-icon' />
-              <span className='h-n-l-last-span'>{cart.length}</span>
-          </Link>
-          <Link className='header-navs-link contact two' to='/'>
+          <Link className='header-navs-link contact two disphide' to='/'>
               <span className='h-n-l-onespan h-n-l-onespan-white '>CONTACT</span>
               <span className='notif'><SupervisorAccountIcon/></span>
-          </Link> 
+          </Link>
+          <Link className='header-navs-link-last cart ' to='checkout'>
+              <span className='h-n-l-onespan h-n-l-onespan-white disphide'>CART</span> 
+              <ShoppingCartIcon className='h-n-l-l-icon' /> 
+              <span className='h-n-l-last-span '>{cart.length}</span>                  
+          </Link>
+          <div onClick={()=>openMenu()} className='menuicn'>
+             <MenuRoundedIcon  className='menuicn-icn' />
+          </div> 
       </div>
-    </div>
+      </div>
+      <div className={open? 'openedmenu rcheader': 'closedmenu rcheader'}>
+          <Link className='rch-link' to='login'>
+              <span className='h-n-l-twospan'>Sign in</span>
+          </Link>
+          <Link className='rch-link' to='/'>
+            <NotificationsIcon className='' /> 
+          </Link>  
+          <Link className='rch-link' to='/'>
+              <span className=''>Contact</span>
+              <span className=''><SupervisorAccountIcon/></span>
+          </Link>
+      </div>
+    </div> 
   )
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,76 @@
-import React from 'react'
-import './Login.css'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react';
+import {useForm} from 'react-hook-form';
+import './Login.css';
 
 function Login() {
-    return (
-       <div className='login'>
-          <Link className='form-logo-link' to='/'>
-             <img src='https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/amazon-512.png' className='login-logo' />
-          </Link>
-           <div className='container-login'>
-               <h1>Sign in</h1>
-               <form className='login-form'>
-                   <span>Username</span>
-                   <input type='text' />
-                   <span>E-mail</span>
-                   <input type='email' />
-                   <span>Password</span>
-                   <input type='password' />
-                   <Link className='' to='/'>
-                       <button>Log In</button>
-                   </Link>
-               </form>
-               <p>By signing in you agree to Amazons conditions of use and sale. please see our Privacy Notice,our Cookies Notice and our Inrest-Based Ads Notice</p>
-               <button className='c-f-but'>Create your Amazon Account</button> 
-           </div>
-       </div>
-    )
+
+  const [peye, setPeye] = useState(false);
+  const [cpeye, setCpeye] = useState(false);
+
+  const {register,handleSubmit,watch, formState:{errors}} = useForm();
+
+  const cp = watch("password",'');
+
+  const changePeye = () =>{
+    setPeye(!peye);
+  }
+  const changeCpeye = () =>{
+    setCpeye(!cpeye);
+  } 
+
+  const onSubmit = (data) => {
+      console.log(data);
+  }
+
+  return (
+    <div className='sect-main-div'>
+      <div className='leftrrow'>
+         <img className='leftrrowimg' src='https://img.freepik.com/free-psd/teal-color-headphone-brand-product-social-media-post-banner_154386-100.jpg?size=626&ext=jpg&ga=GA1.2.1719178491.1616889600' />
+      </div>
+      <div className='bgg-form'>  
+         <form className='formfff' onSubmit={handleSubmit(onSubmit)} >
+          
+              <label>Your Full Name</label>
+              <input placeholder='Enter Your Full Name' type='text' {...register('fullname',{required:true})}  />
+              {errors.fullname && <p className='error'>
+              <i className="fas fa-exclamation-circle"></i>Full name is required</p>}
+              
+              <label>Your Email Address</label>
+              <input   placeholder='Enter Email Address' type='email'  {...register('email',{required:true})}   />
+              {errors.email && <p className='error'>
+              <i className="fas fa-exclamation-circle"></i> Email is required
+              </p>}
+
+              <label>Password</label>
+              <div><input type={peye?'text':'password'}  placeholder='Enter Password'  {...register('password',{required:true,
+                 pattern : { 
+                 value : /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*[!@#$%^&*_-]).{8,}$/ ,   
+                 message: "Your password must have at least 8 characters, one of each of the following: uppercase character (A-Z), lowercase character (a-z), digit (0-9), and symbol (any non-alphanumeric character)", 
+             } 
+             })} />{peye?<i onClick={changePeye} className="fas fa-eye"></i>:<i onClick={changePeye} className="fas fa-eye-slash"></i>}</div>
+              {errors.password?.type === "pattern" && <p className='error'> 
+              <i className="fas fa-exclamation-circle"></i>{errors.password.message}  
+              </p>}
+              {errors.password?.type === "required" && <p className='error'>
+              <i className="fas fa-exclamation-circle"></i>Password is required
+              </p>}
+
+              <label>Confirm password</label>
+              <div><input  placeholder='Confirm your password' type={cpeye?'text':'password'}  {...register('confirmPassword',{required:true,
+                      validate : value => value === cp || "Did not match" ,
+              })}/>{cpeye?<i onClick={changeCpeye} className="fas fa-eye"></i>:<i onClick={changeCpeye} className="fas fa-eye-slash"></i>}</div>
+              {errors.confirmPassword?.type === "validate" && <p className='error'>
+              <i className="fas fa-exclamation-circle"></i>{errors.confirmPassword.message}
+              </p>}
+              {errors.confirmPassword?.type === "required" && <p className='error'>
+              <i className="fas fa-exclamation-circle"></i>Confirm your password
+              </p>}
+
+              <button>Signup</button>
+         </form>
+         </div>
+    </div>
+  )
 }
 
 export default Login;
