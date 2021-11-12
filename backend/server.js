5db060d2bf5a2eaf377e2f24e1b2cd7bc46ad185
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-11-13 03:47:13 +0530

    load breakout

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const mongoose = require('mongoose');
-
+const path = require('path');
 require('dotenv').config();
 
 const app = express();  
@@ -10,16 +10,22 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
- 
-mongoose.connect("mongodb+srv://bhavesh:passwordecom01@ecommerceacc.ilmxs.mongodb.net/ItemsLists?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
-//passwordecom01
+
+const pth = process.env.URI;
+mongoose.connect(pth, { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
+
 
 const addAcc = require('./routes/addAcc'); 
 
 if(process.env.NODE_ENV === "production"){ 
     app.use(express.static("client/build"));
+    app.get('/*',function(req,res){
+        res.sendFile(path.join(__dirname,"client/build/index.html"))
+    })
 }
-
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname,"client/build/index.html"))
+})
 //app.use(createProxyMiddleware('/ac',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom//'}));
 //const apiProxy = createProxyMiddleware('/',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom//auth'});
 //app.use(apiProxy); 
