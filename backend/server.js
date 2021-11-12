247d8acec12c9d1bf93b215ce2d1d3f0d3cd0075
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-11-12 16:03:01 +0530

    json modify

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use('/item', addItems);
 app.use('/ac', addAcc); 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('../build'));
+    app.use(express.static('build'));
 }
 
 app.listen(port, () => {
