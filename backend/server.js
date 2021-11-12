224ea9a1050e4d2f889a0f954d156a392c6f2721
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-11-13 04:05:38 +0530

    uri db

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const pth = process.env.URI;
-mongoose.connect(pth, { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
-
+mongoose.connect("mongodb+srv://bhavesh:passwordecom01@ecommerceacc.ilmxs.mongodb.net/ItemsLists?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
 
 const addAcc = require('./routes/addAcc'); 
 
@@ -23,9 +21,6 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname,"client/build/index.html"))
     })
 }
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname,"client/build/index.html"))
-})
 //app.use(createProxyMiddleware('/ac',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom/'}));
 //const apiProxy = createProxyMiddleware('/',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom/auth'});
 //app.use(apiProxy); 
