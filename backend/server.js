918cd25ceecaa83a8a99beea8c7b99790f1db3ba
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-11-16 01:53:36 +0530

    clusterupdate

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://bhavesh:passwordecom01@ecommerceacc.ilmxs.mongodb.net/ItemsLists?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
+mongoose.connect("use ur mongodb cluster URI", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
 
 const addAcc = require('./routes/addAcc'); 
 
