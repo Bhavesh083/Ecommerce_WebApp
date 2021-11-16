9ac6e9443bfba81ccb31744dc10d14053bf06d3a
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-11-16 21:47:42 +0530

    cluster update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("use ur mongodb cluster URI", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
+mongoose.connect("use your mongodb cluster URI", { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
 
 const addAcc = require('./routes/addAcc'); 
 
@@ -21,9 +21,6 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname,"client/build/index.html"))
     })
 }
-//app.use(createProxyMiddleware('/ac',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom/'}));
-//const apiProxy = createProxyMiddleware('/',{target:'https://bhav-ecommerceapp.herokuapp.com/ecom/auth'});
-//app.use(apiProxy); 
  
 app.use('/ecom', addAcc); 
 
